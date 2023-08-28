275f737a6251526822e45dd415885b4b9b3bcc14
elnurnabivi <elnurnabivi@gmail.com>
2023-08-28 22:31:43 +0300

    Corrected typos

--- jQuery-exercises/2.Calculator/script.js
+++ jQuery-exercises/2.Calculator/script.js
@@ -1,1 +1,23 @@
-$(document).ready(() => {});
+$(document).ready(() => {
+  let result;
+  let screen = $(".screen");
+  screen.val("0");
+
+  $(".btn").click((e) => {
+    if (screen.val() === "0") {
+      screen.val("");
+    }
+    result = e.target.dataset.num;
+    const displayResult = screen.val() + result;
+    screen.val(displayResult);
+  });
+
+  $(".btn-equal").click(() => {
+    result = eval(screen.val());
+    screen.val(result);
+  });
+
+  $(".btn-clear").click(() => {
+    screen.val("0");
+  });
+});
