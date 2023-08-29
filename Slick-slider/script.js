24d69b11abb9cbcbb430550190a585e4e6fb6884
elnurnabivi <elnurnabivi@gmail.com>
2023-08-29 18:02:53 +0300

    Added a slick slider

---
+++ Slick-slider/script.js
@@ -0,0 +1,32 @@
+//Add a slick slider
+$(document).ready(() => {
+  $(".slider").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    // cssEase: "linear",
+    // fade: true,
+    // initialSlide: 3,
+    // centerMode: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
